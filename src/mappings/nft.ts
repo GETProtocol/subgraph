69efdc8ac48c9fb146379813283ba80903886d19
Jack Turnbull <git@jack.codes>
2021-09-09 16:16:49 +0800

    style: add linting & formatting standards
    Setup some starter tooling to enforce code standards around linting and
    formatting. Avoids bikeshedding on code style, just take what prettier
    gives us and do what eslint tells us to do - simple as that!
    
    - Husky and lint-staged added to auto-lint and format changed files
      prior to commit.
    - commitlint added with defaults for commit line formatting.
    - Prettier defaults (with ll 120)
    - Eslint for typescript, inherit TS config from @graphprotocol

--- src/mappings/nft.ts
+++ src/mappings/nft.ts
@@ -1,20 +1,13 @@
-import { BigInt, ByteArray } from "@graphprotocol/graph-ts"
-import { BIG_INT_ONE } from '../constants/index';
+import { BIG_INT_ONE } from "../constants/index";
 import {
-  Contract,
-  ConfigurationChanged,
-  ConfigurationChangedEcon,
-  NFTCheckedIn,
   illegalScan,
   nftClaimed,
-  nftTokenURIEdited,
   primarySaleMint,
   saleCollaterizedIntentory,
-  secondarySale,
   ticketInvalidated,
-  ticketScanned
-} from "../../generated/Contract/Contract"
-import { getProtocol, getProtocolDay, getRelayerDay } from '../entities';
+  ticketScanned,
+} from "../../generated/Contract/Contract";
+import { getProtocol, getProtocolDay, getRelayerDay } from "../entities";
 
 /**
  * NFT CONTRACT V1 --------------------------------------------------------------------
@@ -23,117 +16,114 @@ import { getProtocol, getProtocolDay, getRelayerDay } from '../entities';
 // export function handleConfigurationChangedEcon(event: ConfigurationChangedEcon): void {}
 // export function handleNFTCheckedIn(event: NFTCheckedIn): void { }
 export function handleillegalScan(event: illegalScan): void {
-  let protocol = getProtocol(event)
-  let protocolDay = getProtocolDay(event)
-  let relayerDay = getRelayerDay(event)
+  const protocol = getProtocol();
+  const protocolDay = getProtocolDay(event);
+  const relayerDay = getRelayerDay(event);
 
-  
   protocol.fuel_used.plus(event.params.getUsed);
   protocol.illegal_scans += 1;
-  
+
   protocolDay.fuel_used.plus(event.params.getUsed);
   protocolDay.illegal_scans += 1;
 
   relayerDay.fuel_used.plus(event.params.getUsed);
   relayerDay.illegal_scans += 1;
-  
-  protocol.save()
-  protocolDay.save()
-  relayerDay.save()
+
+  protocol.save();
+  protocolDay.save();
+  relayerDay.save();
 }
 
 export function handlenftClaimed(event: nftClaimed): void {
-  let protocol = getProtocol(event)
-  let protocolDay = getProtocolDay(event)
-  let relayerDay = getRelayerDay(event)
+  const protocol = getProtocol();
+  const protocolDay = getProtocolDay(event);
+  const relayerDay = getRelayerDay(event);
 
   protocol.fuel_used.plus(event.params.getUsed);
   protocol.claims.plus(BIG_INT_ONE);
   protocol.changes.plus(BIG_INT_ONE);
-  
+
   protocolDay.fuel_used.plus(event.params.getUsed);
   protocolDay.claims.plus(BIG_INT_ONE);
   protocolDay.changes.plus(BIG_INT_ONE);
-  
+
   relayerDay.fuel_used.plus(event.params.getUsed);
   relayerDay.claims.plus(BIG_INT_ONE);
   relayerDay.changes.plus(BIG_INT_ONE);
 
-  protocol.save()
-  protocolDay.save()
-  relayerDay.save()
+  protocol.save();
+  protocolDay.save();
+  relayerDay.save();
 }
 
 // export function handlenftTokenURIEdited(event: nftTokenURIEdited): void { }
 
 // NOTE this takes the place of "handleNftMinted", since it doesnt exist on V2 contracts
 export function handleprimarySaleMint(event: primarySaleMint): void {
-  let protocol = getProtocol(event)
-  let protocolDay = getProtocolDay(event)
-  let relayerDay = getRelayerDay(event)
+  const protocol = getProtocol();
+  const protocolDay = getProtocolDay(event);
+  const relayerDay = getRelayerDay(event);
 
   protocol.ticket_value.plus(event.params.primaryPrice);
   protocol.mints.plus(BIG_INT_ONE);
 
   protocolDay.ticket_value.plus(event.params.primaryPrice);
   protocolDay.mints.plus(BIG_INT_ONE);
-  
+
   relayerDay.ticket_value.plus(event.params.primaryPrice);
-  relayerDay.mints.plus(BIG_INT_ONE)
-  
-  protocol.save()
-  protocolDay.save()
-  relayerDay.save()
+  relayerDay.mints.plus(BIG_INT_ONE);
+
+  protocol.save();
+  protocolDay.save();
+  relayerDay.save();
 }
 
-export function handlesaleCollaterizedIntentory(
-  event: saleCollaterizedIntentory
-): void { }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+export function handlesaleCollaterizedIntentory(event: saleCollaterizedIntentory): void {}
 
 // export function handlesecondarySale(event: secondarySale): void { }
 
 export function handleticketInvalidated(event: ticketInvalidated): void {
-  let protocol = getProtocol(event)
-  let protocolDay = getProtocolDay(event)
-  let relayerDay = getRelayerDay(event)
+  const protocol = getProtocol();
+  const protocolDay = getProtocolDay(event);
+  const relayerDay = getRelayerDay(event);
 
   protocol.fuel_used.plus(event.params.getUsed);
   protocol.changes.plus(BIG_INT_ONE);
 
   protocolDay.fuel_used.plus(event.params.getUsed);
   protocolDay.changes.plus(BIG_INT_ONE);
-  
+
   relayerDay.fuel_used.plus(event.params.getUsed);
   relayerDay.changes.plus(BIG_INT_ONE);
 
-  protocol.save()
-  protocolDay.save()
-  relayerDay.save()
+  protocol.save();
+  protocolDay.save();
+  relayerDay.save();
 }
 
 export function handleticketScanned(event: ticketScanned): void {
-  let protocol = getProtocol(event)
-  let protocolDay = getProtocolDay(event)
-  let relayerDay = getRelayerDay(event)
+  const protocol = getProtocol();
+  const protocolDay = getProtocolDay(event);
+  const relayerDay = getRelayerDay(event);
 
   protocol.fuel_used.plus(event.params.getUsed);
   protocol.scans.plus(BIG_INT_ONE);
   protocol.changes.plus(BIG_INT_ONE);
-  
+
   protocolDay.fuel_used.plus(event.params.getUsed);
   protocolDay.scans.plus(BIG_INT_ONE);
   protocolDay.changes.plus(BIG_INT_ONE);
-  
+
   relayerDay.fuel_used.plus(event.params.getUsed);
   relayerDay.scans.plus(BIG_INT_ONE);
   relayerDay.changes.plus(BIG_INT_ONE);
 
-  protocol.save()
-  protocolDay.save()
-  relayerDay.save()
+  protocol.save();
+  protocolDay.save();
+  relayerDay.save();
 }
 
 /**
  * END NFT CONTRACT V1 --------------------------------------------------------------------
  */
-
